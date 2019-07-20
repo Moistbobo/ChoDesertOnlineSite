503ae6d671eeea6fd92c58ee3f44f593864be014
Kevin <moistbobo@outlook.com>
2019-07-20 23:54:32 +0800

    Updates

---
+++ precache-manifest.789a91d0be657eda9ddf165a0db96266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1189ca1ff9a6847c37413a5d2e35a7",
+    "url": "/ChoDesertOnlineSite/index.html"
+  },
+  {
+    "revision": "3fd994a274c4aef18290",
+    "url": "/ChoDesertOnlineSite/static/css/main.e1097edc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ChoDesertOnlineSite/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3fd994a274c4aef18290",
+    "url": "/ChoDesertOnlineSite/static/js/main.df7b9c74.chunk.js"
+  },
+  {
+    "revision": "bd507c832c453515bc07",
+    "url": "/ChoDesertOnlineSite/static/js/runtime~main.4ab4881b.js"
+  },
+  {
+    "revision": "d94d423298d3bda946132e8e4fad5cc5",
+    "url": "/ChoDesertOnlineSite/static/media/bg.d94d4232.jpg"
+  }
+]);
